169eaf331727d23b4488e5fc641bd815a28e2054
Bourgeois <133302526+bourgeois46@users.noreply.github.com>
2023-05-29 15:44:30 +0900

    Create ExceptionHandling.java

---
+++ week9/ExceptionHandling.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class ExceptionHandling {
+  public static int quotient(int a, int b) throws ArithmeticException { // 예외를 던지는 메소드 -> try문에서 호출
+	  return a/b;
+  }
+  
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    
+    while(true) {
+      try {
+        System.out.print("Enter dividend: ");
+        int dividend = sc.nextInt();
+        System.out.print("Enter divisor: ");
+        int divisor = sc.nextInt();
+        
+        System.out.println("Quotient is " + quotient(dividend, divisor));
+        break; // 무한 루프문 try문에서 써줘야 됨
+      }
+      
+      catch (ArithmeticException e) {
+        System.out.println("Cannot divide by 0. Try agin.");
+      }
+      
+      catch (IputMismatchException e) {
+         System.out.println("You must enter integers. Try agin.");
+         sc.next(); // 잘못된 입력 제거하기
+      }
+    }
+    sc.close();
+  } 
+}
+
+// 다른 클래스에 메소드 따로 정리
+public static int quotient(int a, int b) throw Arithmetic
